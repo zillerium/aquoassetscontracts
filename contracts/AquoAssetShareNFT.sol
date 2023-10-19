e8ec0930a7b9a57f1ca5697919dc69fe71502851
cryptotrader2023xx@proton.me <cryptotrader2023xx@proton.me>
2023-10-19 09:39:05 +0000

    nft

--- contracts/AquoAssetShareNFT.sol
+++ contracts/AquoAssetShareNFT.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// deploy first in the sequence of contracts - AquoAssetShareNFT.sol, AquoAssetManager.sol, then AquoUserProxy.sol
+// deploy first in the sequence of contracts -  AquoAssetManager.sol, then AquoUserProxy.sol
 
-contract AquoAssetShareNFT is ERC721Enumerable, Ownable {
-    constructor() ERC721("Asset Share Token", "AST") {}
-
+contract AquoAssetShareNFT is ERC721URIStorage, Ownable {
+    constructor() ERC721("Asset Share Token", "AST") {
+     
+    }
+ 
     // Mint new NFTs
-    function mint(address to) external onlyOwner returns (uint256) {
-        uint256 tokenId = totalSupply() + 1;
-        _mint(to, tokenId);
-        return tokenId;
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        string calldata _uri
+    ) external onlyOwner  {
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, _uri);
     }
 }
+
