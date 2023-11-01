c2e94afef5adf5c14499fd13840882a595b3da17
Trevor <sales@merrows.co.uk>
2023-11-01 15:45:53 +0000

    contracts

--- newcontracts/AquoProspectusList2v3.sol
+++ newcontracts/AquoProspectusList2v3.sol
@@ -1,32 +1,57 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-contract AssetContract {
+contract ProspectusRegistry {
+    
+    // Updated struct to hold the IPFS address for the prospectus and image, and the proposer's wallet address
+    struct ProspectusRecord {
+        string prospectusCid;
+        string imageCid;
+        address proposer;
+    }
+    
+    // Array to hold all the ProspectusRecord entries
+    ProspectusRecord[] public records;
+    
+    // Mappings to check for unique IPFS entries for both prospectus and image
+    mapping(string => bool) private prospectusCidExists;
+    mapping(string => bool) private imageCidExists;
 
-    address public owner;
-    mapping(address => uint256) shareBalances;
-    uint256 public totalShares;
+    // Updated function to add a new record with both prospectus and image CIDs
+    function addRecord(string memory _prospectusCid, string memory _imageCid) public {
+        // Ensure the IPFS addresses haven't been added before
+       // require(!prospectusCidExists[_prospectusCid], "Prospectus CID already exists");
+       // require(!imageCidExists[_imageCid], "Image CID already exists");
+        
+        if(prospectusCidExists[_prospectusCid] || imageCidExists[_imageCid]) {
+            return;
+        }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner");
-        _;
+        // Create a new record and push it to the array
+        records.push(ProspectusRecord({
+            prospectusCid: _prospectusCid,
+            imageCid: _imageCid,
+            proposer: msg.sender
+        }));
+        
+        // Mark the IPFS addresses as added
+        prospectusCidExists[_prospectusCid] = true;
+        imageCidExists[_imageCid] = true;
     }
 
-// this is deployed from a contract and hence msg.sender would be the sending contract, so _owner is used
-    constructor(uint256 _initialShares, address _owner) {
-        owner = _owner;
-        shareBalances[owner] = _initialShares;
-        totalShares = _initialShares;
+    // Get the total number of records
+    function getRecordCount() public view returns (uint) {
+        return records.length;
+    }
+    
+    // Get a specific record by index
+    function getRecord(uint _index) public view returns (ProspectusRecord memory) {
+        require(_index < records.length, "Index out of bounds");
+        return records[_index];
     }
 
-    function balanceOf(address _user) public view returns (uint256) {
-        return shareBalances[_user];
-    }
-
-    function transferShares(address _to, uint256 _amount) public {
-        require(shareBalances[msg.sender] >= _amount, "Insufficient shares");
-        shareBalances[msg.sender] -= _amount;
-        shareBalances[_to] += _amount;
+    function getAllRecords() public view returns (ProspectusRecord[] memory) {
+        return records;
     }
 }
 
