66f13501f5f2a519d591753bc85fda24d8403b41
Trevor <sales@merrows.co.uk>
2023-11-05 03:08:02 +0000

    prospectus

--- newcontracts/AquoProspectusList2v3.sol
+++ newcontracts/AquoProspectusList2v3.sol
@@ -16,6 +16,8 @@ contract ProspectusRegistry {
     // Mappings to check for unique IPFS entries for both prospectus and image
     mapping(string => bool) private prospectusCidExists;
     mapping(string => bool) private imageCidExists;
+    mapping(string => ProspectusRecord) private cidToProspectusRecord; // New mapping
+
 
     // Updated function to add a new record with both prospectus and image CIDs
     function addRecord(string memory _prospectusCid, string memory _imageCid) public {
@@ -27,16 +29,19 @@ contract ProspectusRegistry {
             return;
         }
 
-        // Create a new record and push it to the array
-        records.push(ProspectusRecord({
-            prospectusCid: _prospectusCid,
-            imageCid: _imageCid,
-            proposer: msg.sender
-        }));
+        ProspectusRecord memory newRecord = ProspectusRecord({
+        prospectusCid: _prospectusCid,
+        imageCid: _imageCid,
+        proposer: msg.sender
+        });
+
+        records.push(newRecord);       
         
         // Mark the IPFS addresses as added
         prospectusCidExists[_prospectusCid] = true;
         imageCidExists[_imageCid] = true;
+        cidToProspectusRecord[_prospectusCid] = newRecord; // Store the record in the new mapping
+
     }
 
     // Get the total number of records
@@ -52,6 +57,11 @@ contract ProspectusRegistry {
 
     function getAllRecords() public view returns (ProspectusRecord[] memory) {
         return records;
+    }
+
+    function getProspectusRecord(string memory _prospectusCid) public view returns (ProspectusRecord memory) {
+        require(prospectusCidExists[_prospectusCid], "Prospectus CID does not exist");
+        return cidToProspectusRecord[_prospectusCid];
     }
 }
 
