c95d64ecd4328b61f7424243b24a13e171dc3d4b
zillerium <23511764+zillerium@users.noreply.github.com>
2023-11-09 09:28:11 +0000

    Update AquoAssetTransferv3.sol

--- newcontracts/AquoAssetTransferv3.sol
+++ newcontracts/AquoAssetTransferv3.sol
@@ -2,21 +2,24 @@
 pragma solidity ^0.8.20;
 
 contract AssetContract {
-
     address public owner;
-    mapping(address => uint256) shareBalances;
-    uint256 public totalShares;
+    mapping(address => uint256) private shareBalances; // Mapping to keep track of balances
+    address[] private holderAddresses; // Array to keep track of all addresses ever held shares
+
+    struct Holder {
+        address addr;
+        uint256 balance;
+    }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
         _;
     }
 
-// this is deployed from a contract and hence msg.sender would be the sending contract, so _owner is used
     constructor(uint256 _initialShares, address _owner) {
         owner = _owner;
-        shareBalances[owner] = _initialShares;
-        totalShares = _initialShares;
+        shareBalances[_owner] = _initialShares;
+        holderAddresses.push(_owner); // Add the owner to the holder addresses array
     }
 
     function balanceOf(address _user) public view returns (uint256) {
@@ -27,6 +30,17 @@ contract AssetContract {
         require(shareBalances[msg.sender] >= _amount, "Insufficient shares");
         shareBalances[msg.sender] -= _amount;
         shareBalances[_to] += _amount;
+
+        if (shareBalances[_to] == _amount) { // if the balance was zero before transfer
+            holderAddresses.push(_to); // Add the recipient to the holder addresses array
+        }
+    }
+
+    function getHolders() public view returns (Holder[] memory) {
+        Holder[] memory holders = new Holder[](holderAddresses.length);
+        for (uint256 i = 0; i < holderAddresses.length; i++) {
+            holders[i] = Holder(holderAddresses[i], shareBalances[holderAddresses[i]]);
+        }
+        return holders;
     }
 }
-
